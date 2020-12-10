0e794c4e97c4386e4cc6e0cf318010aee71c86c7
Eimantas <eim.klimas@gmail.com>
2020-12-10 21:36:33 +0200

    Feature: basic member list add

--- ui/src/components/CreateClub.js
+++ ui/src/components/CreateClub.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import {VALUES} from "../constants/values";
+import "../styles/CreateClub.css"
 
 const CreateClub = () => {
+  const [memberName, setName] = React.useState("");
+  const [memberSurname, setSurname] = React.useState("");
+  const [memberList, setMemberList] = React.useState([]);
+
+  const handleChange = (event) => {
+    switch (event.target.name.toUpperCase()) {
+      case VALUES.MEMBERNAME:
+        setName(event.target.value);
+        break;
+      case VALUES.MEMBERSURNAME:
+        setSurname(event.target.value);
+        break;
+      default:
+        console.log("Unknown event")
+    }
+  };
+
+  const handleAddMember = (event) => {
+    let newMember = {
+      "name": memberName,
+      "surname": memberSurname
+    };
+    let newList = memberList.concat(newMember);
+
+    setMemberList(newList);
+    setName("");
+    setSurname("");
+  }
+
   return(
     <Grid container spacing={2} className="form">
+      <Paper elevation={3}>
+        <Grid item xs={12} className="form-item">
+          <TextField
+            label="Name"
+            value={memberName}
+            name={VALUES.MEMBERNAME}
+            size="small"
+            variant="standard"
+            onChange={handleChange}/>
+        </Grid>
+        <Grid item xs={12} className="form-item">
+          <TextField
+            label="Surname"
+            value={memberSurname}
+            name={VALUES.MEMBERSURNAME}
+            size="small"
+            variant="standard"
+            onChange={handleChange}/>
+        </Grid>
+        <IconButton aria-label="delete" onClick={handleAddMember}>
+          <AddCircleIcon fontSize="large" />
+        </IconButton>
+      </Paper>
       <Grid item xs={12}>
-        <TextField label="Club Name" name="clubName" size="small" variant="standard" />
+      <Paper elevation={3}>
+
+      </Paper>
       </Grid>
     </Grid>
   )
