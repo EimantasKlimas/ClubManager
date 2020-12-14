e11b487f4d8411dbaa19b0c66508cd373b6cec72
Eimantas <eim.klimas@gmail.com>
2020-12-14 03:41:15 +0200

    Correction: actor not completing futures

--- app/actors/MemberActor.scala
+++ app/actors/MemberActor.scala
@@ -5,8 +5,10 @@ import akka.stream.Materializer
 import repositories.MemberRepository
 import repositories.data.Model.MemberDTO
 
+import java.util.concurrent.TimeUnit
 import javax.inject._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 object MemberActor {
   case object Ack
@@ -34,11 +36,9 @@ class MemberActor @Inject()(memberRepository: MemberRepository)(implicit ec: Exe
       log.error(ex, "Stream failed!")
   }
 
-  def saveMember(member: MemberDTO): Future[Unit] = {
-    for {
-      _ <- memberRepository.saveMember(member)
-    } yield {
+  def saveMember(member: MemberDTO): Unit = {
+    //TODO think of better way of doing
+    Await.result(memberRepository.saveMember(member), Duration.create(1, TimeUnit.SECONDS))
       sender() ! Ack
     }
-  }
 }
